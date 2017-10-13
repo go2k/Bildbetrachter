0ecb300ea772f7e4f4972b46c76276ed08591848
Michael König <m.koenig@go2k.de>
2017-10-13 11:24:50 +0200

    ende dialog

--- src/com/sabel/bildbetrachter/Testklasse.java
+++ src/com/sabel/bildbetrachter/Testklasse.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class Testklasse extends JFrame{
 
@@ -12,25 +14,78 @@ public class Testklasse extends JFrame{
 
     public Testklasse() throws HeadlessException {
         super();
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         this.setSize(300,300);
         this.setLocationRelativeTo(null);
-        this.setVisible(true);
+
 
         button = new Button("klick");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                klick();
+                endDialog();
             }
         });
+
         this.add(button);
+
+
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                endDialog();
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+
+
+        this.setVisible(true);
+
 
     }
 
-    private void klick() {
+    // https://docs.oracle.com/javase/tutorial/uiswing/components/dialog.html#overview
 
-        //JOptionPane.showConfirmDialog(null , "Nachricht", "Titel");
+    private void endDialog() {
+
+        int n = JOptionPane.showConfirmDialog(
+                this,
+                "Wollen Sie das Programm wirklich beenden?",
+                "Beenden?",
+                JOptionPane.YES_NO_OPTION);
+
+        if (n == 0) {
+            System.exit(0);
+        }
 
     }
 
